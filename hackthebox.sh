72b3814c47533b9a4882e465438ef01bb9d37974
Castilho <rafael.castilho@tutanota.com>
2020-11-22 04:57:44 +0000

    Add files via upload

---
+++ hackthebox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to download automatically
+# challenges to our remote machine
+
+# Variables
+CHALLENGE=$1
+HACKTHEBOX_SESSION="HACKTHEBOX--SESSION-COOKIE"
+
+# Auto trasnfer, unzip and move to right folder
+wget -q --header="Cookie: hackthebox_session=$HACKTHEBOX_SESSION" https://www.hackthebox.eu/home/challenges/download/$CHALLENGE
+mv $CHALLENGE $CHALLENGE.zip
+unzip -q -P hackthebox $CHALLENGE.zip -d ~/hackthebox
+rm $CHALLENGE.zip
